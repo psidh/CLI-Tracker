5a2c4954cfc5467eeb4c7f5a17ec25ee093d6e02
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-10-09 11:09:36 +0530

    [bug] fixed : path to `text` file
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/tasks/create.rs
+++ src/tasks/create.rs
@@ -1,7 +1,8 @@
 use chrono::{NaiveDate, Utc};
 use std::{
-    fs::OpenOptions,
+    fs::{create_dir_all, OpenOptions},
     io::{self, Write},
+    path::Path,
 };
 
 pub struct Task {
@@ -21,18 +22,18 @@ impl Task {
         }
     }
 
-    pub fn display(&self) {
-        println!("Task ID: {}", self.id);
-        println!("Title: {}", self.title);
-        println!("Description: {}", self.description);
-        println!("Date: {}", self.date);
-    }
-
     // Write task to file
     pub fn write_to_file(&self) {
         let timestamp = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
         let file_path = "db/tasks.txt";
 
+        // Ensure the db directory exists
+        let db_path = Path::new("db");
+        if !db_path.exists() {
+            create_dir_all(db_path).expect("Failed to create db directory");
+        }
+
+        // Open the file and append the task
         let mut file = OpenOptions::new()
             .create(true)
             .append(true)
